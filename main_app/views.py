b78f773e78005d6846a8fd13f265e9fbadcc68ed
Neha Bhole <nehasbhole@gmail.com>
2022-12-12 17:09:04 -0500

    add home, about routes

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+# Add the Cat class & list and view function below the imports
+class Goal:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, reason, what, deadline):
+    self.name = name
+    self.reason = reason
+    self.what = what
+    self.deadline = deadline
+
+goals = [
+  Goal('Read', 'Gain perspective', 'Read 10 mins a day', 'Everyday'),
+  Goal('Save for Europe Trip', 'I want to see Italy', 'Save $5000', '2 months'),
+]
 
 # Create your views here.
+def home(request):
+  return HttpResponse('<h1>Welcome to Goal Collector</h1>')
+
+def about(request):
+  return render(request, 'about.html')
+
+def goals_index(request):
+  return render(request, 'goals/index.html', { "goals": goals })
